dd087e13b1daa54f519344b88756502aa2231a09
asa <asa.lu@outlook.com>
2026-09-08 18:17:26 +0800

    fix: 日期统一改为0000-00-00，表格显示为—

--- src/data/places.ts
+++ src/data/places.ts
@@ -12,112 +12,112 @@ const data: Place[] = [
     name: "新加坡",
     lat: 1.3521,
     lng: 103.8198,
-    date: "待补充",
+    date: "0000-00-00",
     category: "旅游"
   },
   {
     name: "深圳",
     lat: 22.5431,
     lng: 114.0579,
-    date: "待补充",
+    date: "0000-00-00",
     category: "旅游"
   },
   {
     name: "长沙",
     lat: 28.2282,
     lng: 112.9388,
-    date: "待补充",
+    date: "0000-00-00",
     category: "旅游"
   },
   {
     name: "成都",
     lat: 30.5728,
     lng: 104.0668,
-    date: "2024-08-20",
+    date: "0000-00-00",
     category: "旅游"
   },
   {
     name: "南京",
     lat: 32.0603,
     lng: 118.7969,
-    date: "待补充",
+    date: "0000-00-00",
     category: "旅游"
   },
   {
     name: "武汉",
     lat: 30.5928,
     lng: 114.3055,
-    date: "待补充",
+    date: "0000-00-00",
     category: "旅游"
   },
   {
     name: "九江",
     lat: 29.7050,
     lng: 116.0019,
-    date: "待补充",
+    date: "0000-00-00",
     category: "旅游"
   },
   {
     name: "岳阳",
     lat: 29.3572,
     lng: 113.1289,
-    date: "待补充",
+    date: "0000-00-00",
     category: "旅游"
   },
   {
     name: "襄阳",
     lat: 32.0420,
     lng: 112.1441,
-    date: "待补充",
+    date: "0000-00-00",
     category: "旅游"
   },
   {
     name: "洛阳",
     lat: 34.6197,
     lng: 112.4540,
-    date: "待补充",
+    date: "0000-00-00",
     category: "旅游"
   },
   {
     name: "济南",
     lat: 36.6512,
     lng: 116.9970,
-    date: "待补充",
+    date: "0000-00-00",
     category: "旅游"
   },
   {
     name: "烟台",
     lat: 37.4638,
     lng: 121.4479,
-    date: "待补充",
+    date: "0000-00-00",
     category: "旅游"
   },
   {
     name: "北京",
     lat: 39.9042,
     lng: 116.4074,
-    date: "2023-10-01",
+    date: "0000-00-00",
     category: "旅游"
   },
   {
     name: "合肥",
     lat: 31.8206,
     lng: 117.2272,
-    date: "待补充",
+    date: "0000-00-00",
     category: "旅游"
   },
   {
     name: "黄石",
     lat: 30.1996,
     lng: 115.0385,
-    date: "待补充",
+    date: "0000-00-00",
     category: "旅游"
   },
   {
     name: "鄂州",
     lat: 30.3908,
     lng: 114.8948,
-    date: "待补充",
+    date: "0000-00-00",
     category: "旅游"
   }
 ]
